a6102ee0048c6a9398afd31bde17aa11bc472478
Arka Kundu <arka99325@gmail.com>
2025-03-19 00:49:22 +0530

    Resolved merge conflicts

--- src/test/java/com/bookStore/BookStoreApplicationTests.java
+++ src/test/java/com/bookStore/BookStoreApplicationTests.java
@@ -3,7 +3,7 @@ package com.bookStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BookStoreApplicationTests {
 
 	@Test
